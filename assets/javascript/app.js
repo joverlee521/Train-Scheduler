b97cb27b17271403aaf31b038b2912194eee96d6
J.Lee <joverlee521@gmail.com>
2018-09-19 13:19:28 -0700

    added basic format of webpage and initialized firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCSBtmL2JihCFsiqOve95k60rFtGaZenOY",
+    authDomain: "train-scheduler-c1a68.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c1a68.firebaseio.com",
+    projectId: "train-scheduler-c1a68",
+    storageBucket: "train-scheduler-c1a68.appspot.com",
+    messagingSenderId: "1067464721353"
+  };
+  firebase.initializeApp(config);
