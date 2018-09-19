2f422bf5f07835db29b0b5f300849f892e743589
J.Lee <joverlee521@gmail.com>
2018-09-19 14:40:09 -0700

    store input into Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,5 +6,41 @@ var config = {
     projectId: "train-scheduler-c1a68",
     storageBucket: "train-scheduler-c1a68.appspot.com",
     messagingSenderId: "1067464721353"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTime = 0;
+var frequency = 0;
+
+$("#submitInput").on("click", function(){
+    event.preventDefault();
+    trainName = $("#trainNameInput").val();
+    destination = $("#destinationInput").val();
+    firstTime = $("#firstTimeInput").val();
+    frequency = parseInt($("#frequencyInput").val());
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency
+    });
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTimeInput").val("");
+    $("#frequencyInput").val("");
+})
+
+database.ref().on("child_added", function(data){
+    console.log(data.val());
+    var newRow = $("<tr>");
+    var newTrainName = $("<td>").text(data.val().trainName);
+    var newDestination = $("<td>").text(data.val().destination);
+    var newFrequency = $("<td>").text(data.val().frequency);
+    newRow.append(newTrainName,newDestination,newFrequency);
+    $("#trainSchedule").append(newRow);
+}, function(error){
+    console.log("There was an error " + error.code);
+})
